da5cd0215714ce6393759cd1610b570ab9c01020
Javier Cejudo <javier@javiercejudo.com>
2016-02-10 01:48:17 +1100

    refactor: functional style with ramda

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 
 'use strict';
 
-var SEPARATOR = ':';
+var R = Object.freeze({
+  pipe: require('ramda/src/pipe'),
+  split: require('ramda/src/split'),
+  reverse: require('ramda/src/reverse'),
+  map: require('ramda/src/map'),
+  addIndex: require('ramda/src/addIndex'),
+  reduce: require('ramda/src/reduce')
+});
 
-function smhToSeconds(pUnit, cUnit, index) {
-  return pUnit + cUnit * Math.pow(60, index);
-}
-
-function hmsTosmh(str) {
-  return str.split(SEPARATOR)
-    .reverse()
-    .map(Number);
-}
-
-module.exports = function(runtime) {
-  return hmsTosmh(runtime).reduce(smhToSeconds);
-};
+module.exports = R.pipe(
+  R.split(':'),
+  R.reverse,
+  R.map(Number),
+  R.addIndex(R.reduce)(function(acc, val, index) {
+    return acc + val * Math.pow(60, index);
+  }, 0)
+);
